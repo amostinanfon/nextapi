4fb679909fa6e56932711d867d3d9b97d9d8bae9
amostinanfon <amostinanfon17@gmail.com>
2023-04-21 14:10:01 +0100

    working on postmark

--- config/site.ts
+++ config/site.ts
@@ -2,8 +2,7 @@ import { SiteConfig } from "@/types"
 
 export const siteConfig: SiteConfig = {
   name: "Bolo",
-  description:
-    "Search, find and post a job",
+  description:"Search, find and post a job",
     url:'localhost:3000'
 //   url: "https://nextapi-rouge.vercel.app/",
 //   ogImage: "https://tx.shadcn.com/og.jpg",
