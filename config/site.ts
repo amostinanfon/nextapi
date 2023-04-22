162bcb44c14b4c116a90740a4fb6246a8e282ebd
amostinanfon <amostinanfon17@gmail.com>
2023-04-22 16:30:18 +0100

    another great commit

--- config/site.ts
+++ config/site.ts
@@ -3,8 +3,7 @@ import { SiteConfig } from "@/types"
 export const siteConfig: SiteConfig = {
   name: "Bolo",
   description:"Search, find and post a job",
-    url:'localhost:3000'
-//   url: "https://nextapi-rouge.vercel.app/",
+  url: "https://nextapi-rouge.vercel.app/",
 //   ogImage: "https://tx.shadcn.com/og.jpg",
 //   links: {
 //     twitter: "https://twitter.com/shadcn",
