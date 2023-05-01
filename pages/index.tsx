e88256a9570d81c8fedcc8a408cadf1eff2e01ef
amostinanfon <amostinanfon17@gmail.com>
2023-05-01 11:47:29 +0100

    working on pagination 5

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import InfoModal from '@/components/InfoModal';
 import useMovieList from '@/hooks/useMovieList';
 import useFavorites from '@/hooks/useFavorites';
 import useInfoModalStore from '@/hooks/useInfoModalStore';
+import Paginate from './paginate';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -41,6 +42,7 @@ const Home = () => {
         <MovieList title="Trending Now" data={movies} />
         <MovieList title="My List" data={favorites} />
       </div>
+      <Paginate dataP={[]} title={''} />
     </>
   )
 }
